4d5c08ca12e68049484cac9b3368b51e2f81caa8
Antzelok <akermezo@yahoo.com>
2025-11-18 13:18:16 +0200

    Spinner component

---
+++ app/components/spinner.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+const Spinner = () => {
+  return (
+    <div className="animate-spin h-6 w-6 border-2 border-gray-400 border-t-transparent rounded-full"></div>
+  );
+};
+
+export default Spinner;
