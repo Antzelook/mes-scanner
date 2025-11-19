43686810318221ea1c2306c97540fe9f26ef94fe
Antzelok <akermezo@yahoo.com>
2025-11-19 09:30:39 +0200

    Add dotenv to project

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -1,6 +1,6 @@
 export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "Mes-Digital";
 export const APP_DESCRIPTION =
   process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
-  "";
+  "Digital solutions for your buisness needs ";
 export const SERVER_URL =
   process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:3000";
