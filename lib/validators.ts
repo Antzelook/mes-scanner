6cdcdce9811215efeb6ec5b1f431d7300b6bb7f5
Antzelok <akermezo@yahoo.com>
2025-11-20 13:27:56 +0200

    validators fixes

--- lib/validators.ts
+++ lib/validators.ts
@@ -1,8 +1,8 @@
 import { z } from "zod";
 
 export const errorSchema = z.object({
-  latitude: z.float32("Το πεδίο είναι υποχρεωτικό"),
-  longtitude: z.float32("Το πεδίο είναι υποχρεωτικό"),
+  latitude: z.number().min(1, "Το πεδίο είναι υποχρεωτικό"),
+  longitude: z.number().min(1, "Το πεδίο είναι υποχρεωτικό"),
   serialNumber: z
     .string()
     .min(1, "Το πεδίο είναι υποχρεωτικό")
