3a7909c3054a4ad0eac3a4978ef5f05929c038e9
Antzelok <akermezo@yahoo.com>
2025-11-28 12:25:22 +0200

    Admin sign in validator

--- lib/validators.ts
+++ lib/validators.ts
@@ -4,9 +4,14 @@ export const errorFormSchema = z.object({
   date: z.string(),
   latitude: z.number().min(1, "Το πεδίο είναι υποχρεωτικό"),
   longitude: z.number().min(1, "Το πεδίο είναι υποχρεωτικό"),
-  serialNumber: z.string().min(1,  "Το πεδίο είναι υποχρεωτικό"),
-  deveui: z.string().min(1,  "Το πεδίο είναι υποχρεωτικό"),
+  serialNumber: z.string().min(1, "Το πεδίο είναι υποχρεωτικό"),
+  deveui: z.string().min(1, "Το πεδίο είναι υποχρεωτικό"),
   types: z.array(z.string()).optional(),
   actions: z.array(z.string()).optional(),
   comment: z.string().optional(),
+});
+
+export const adminSgnInSchema = z.object({
+  email: z.email("Λανθασμένο email"),
+  password: z.string().min(1, "Το πεδίο είναι υποχρεωτικό"),
 });
